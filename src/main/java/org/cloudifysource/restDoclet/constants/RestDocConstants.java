0c4562572d83717a61d857a671f0c5ac3f295ed2
yaelpeled <yael@gigaspaces.com>
2012-10-28 12:10:23 +0200

    CLOUDIFY-1035 Added copyright notice.

--- src/main/java/org/cloudifysource/restDoclet/constants/RestDocConstants.java
+++ src/main/java/org/cloudifysource/restDoclet/constants/RestDocConstants.java
@@ -1,12 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.cloudifysource.restDoclet.constants;
 
 
-
+/**
+ * 
+ * @author yael
+ *
+ */
 public class RestDocConstants {
 	public static final String DOCLET_FLAG = "-doclet";
 	
 	public static final String SOURCE_PATH_FLAG = "-sourcepath";
-	public static final String SOURCES_PATH = "../restful/src/main/java";
+	public static final String SOURCES_PATH = "../../workspaceCloudify/cloudify/restful/src/main/java";
 	public static final String CONTROLLERS_PACKAGE = "org.cloudifysource.rest.controllers";
 	
 	public static final String SERVICE_CONTROLLER_CLASS_NAME = "org.cloudifysource.rest.controllers.ServiceController";
